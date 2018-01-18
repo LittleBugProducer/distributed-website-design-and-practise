a282ab2766ffdd5d0f416d4e5a6293b2fad0272e
LittleBugProducer <tobe1num@163.com>
2018-01-18 15:43:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# distributed-website-design-and-practise
---
+++ src/chap1/p7/Test.java
@@ -0,0 +1,8 @@
+package chap1.p7;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
